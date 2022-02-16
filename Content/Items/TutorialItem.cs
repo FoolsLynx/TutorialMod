1231a30b1759d8b0f6108eb0ca43031691379d89
FoolsLynx <FoolsLynx@users.noreply.github.com>
2022-02-16 14:24:27 +0000

    Added the TutorialItem and TutorialSword from Episode 2

---
+++ Content/Items/TutorialItem.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace TutorialMod.Content.Items
+{
+    internal class TutorialItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutorial Item");
+            Tooltip.SetDefault("This is a Tutorial Material\nThis is line 2");
+            // This accesses the creative catalog
+            // Setting the research number to 100 before it can be fully accessed
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;    // Hitbox Width from Bottom Center
+            Item.height = 16;   // Hitbox Height form Bottom Center
+
+            Item.value = Item.buyPrice(copper: 5); // Value of the Item 120 = Silver: 1, Cooper: 20
+            Item.maxStack = 999;
+        }
+
+    }
+}
---
+++ Content/Items/Weapons/TutorialSword.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace TutorialMod.Content.Items.Weapons
+{
+    internal class TutorialSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutorial Sword");
+            Tooltip.SetDefault("This is a modded Broadsword");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Hitbox
+            Item.width = 32;
+            Item.height = 32;
+
+            // Use and Animation Style
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+            
+            // Damage Values
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 20;
+            Item.knockBack = 3.5f;
+            Item.crit = 5;
+
+            // Misc
+            Item.value = Item.buyPrice(silver: 80, copper: 50);
+            Item.rare = ItemRarityID.Blue;
+
+            // Sound
+            Item.UseSound = SoundID.Item1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<TutorialItem>(), 8) // AddIngredient takes ItemID, then Quantity
+                .AddTile(TileID.Anvils) // AddTile takes the TileID
+                .Register(); // Register registers the item
+        }
+    }
+}
